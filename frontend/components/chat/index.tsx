23ef5971757b97211ecff6ad1d891102454267da
Dawson <dawsonpowell07@gmail.com>
2026-02-13 22:16:38 -0500

    fixed build errors

--- frontend/components/chat/index.tsx
+++ frontend/components/chat/index.tsx
@@ -1,4 +1,1 @@
-export { CustomUserMessage } from "./CustomUserMessage";
-export { CustomAssistantMessage } from "./CustomAssistantMessage";
 export { CustomInput } from "./CustomInput";
-export { CustomMessages } from "./CustomMessages";
